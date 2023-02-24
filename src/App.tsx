020a9d9c54dbee4fcc234358665d5138dd6e0f2a
Sikstep <Sikstep1988@gmail.com>
2023-02-24 17:57:26 +0300

    addition lesson #4 finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,65 @@
 import React from 'react';
 import './App.css';
 import {SuperButton} from './Components/SuperButton';
+import {v1} from 'uuid';
+import {SuperTodolist} from './Components/SuperTodolist';
 
+export type TasksType = {
+    id: string
+    title: string
+    isDone: boolean
+
+}
 
 function App() {
+
+    const tasks: TasksType[] = [
+        {id: v1(), title: 'HTML&CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'ReactJS', isDone: false},
+    ];
+
     return (
         <div>
-            <SuperButton callBack={ () => {} } color={'red'}>MyNameIsButton</SuperButton>
+            <SuperButton callBack={() => {
+            }} color={'red'}>Red Super Button</SuperButton>
+            <SuperButton callBack={() => {
+            }} color={'secondary'}>Without color Supper Button</SuperButton>
+            <SuperButton callBack={() => {
+            }} disabled>Without color Supper Button</SuperButton>
+            <SuperTodolist tasks={tasks}>
+                <SuperButton callBack={() => {
+                }} color={'red'}>Red Super Button</SuperButton>
+                <SuperButton callBack={() => {
+                }} color={'secondary'}>Without color Supper Button</SuperButton>
+                <SuperButton callBack={() => {
+                }} disabled>Without color Supper Button</SuperButton>
+            </SuperTodolist>
+
+            <SuperTodolist tasks={tasks}>
+                <SuperButton callBack={() => {
+                }} color={'red'}>Red Super Button</SuperButton>
+                <SuperButton callBack={() => {
+                }} color={'secondary'}>Without color Supper Button</SuperButton>
+                <div>INFO</div>
+                <div>INFO</div>
+                <div>INFO</div>
+                <div>INFO</div>
+                <div>INFO</div>
+            </SuperTodolist>
+
+            <SuperTodolist tasks={tasks}>
+
+                <div>INFO</div>
+                <div>INFO</div>
+                <div>INFO</div>
+                <div>INFO</div>
+                <div>INFO</div>
+            </SuperTodolist>
+
         </div>
     );
 }
-
 
 
 export default App;
--- src/Components/SuperButton.tsx
+++ src/Components/SuperButton.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import s from './SuperButton.module.css'
+import {isDisabled} from '@testing-library/user-event/dist/utils';
+
 type PropsType = {
     callBack: () => void
-    color: string
+    color?: string
     children: React.ReactNode
     size?: 'big'
+    disabled?: boolean
 }
 
-export const SuperButton: React.FC<PropsType> = ( props) => {
+export const SuperButton: React.FC<PropsType> = (props) => {
 
-    const {callBack, color, children, ...otherProps} = props;
+    const {callBack, color, children, disabled, ...otherProps} = props;
     const onClickHandler = () => {
         callBack()
     }
-    console.log(otherProps.size)
+
+    // const finalClassName = `${s.button} ${s.default}`;
+
+    // const finalClassName = `${s.button}
+    // ${color === 'red' ? s.red : s.default}
+    // ${disabled ? s.disabled : ''}`;
+
+    const finalClassName = `${s.button} 
+    // ${color === 'red' ? s.red : color === 'secondary' ? s.secondary : s.default}
+    // ${disabled ? s.disabled : ''}`;
+
     return (
         <div>
-    <button onClick={onClickHandler}>{children}</button>
+            <button onClick={onClickHandler} className={finalClassName}>{children}</button>
         </div>
     );
+
 };
+
+
+// const finalClassName = s.button
+//     + (disabled
+//         ? ' ' + s.disabled
+//         : color === 'red'
+//             ? ' ' + s.red
+//             : color === 'secondary'
+//                 ? ' ' + s.secondary
+//                 : ' ' + s.default)
+//     + (className ? ' ' + className : '')
 
 // export const SuperButton: React.FC<PropsType> = ( {callBack, color, children, ...props} ) => {
 //     const onClickHandler = () => {
